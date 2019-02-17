387f8d0be413658b08f0660714d3cb9a51edcdc7
Ahmed <aiabdalla5050@gmail.com>
2019-02-17 03:27:11 -0800

    Fix syntax errors

--- src/background.js
+++ src/background.js
@@ -1,31 +1,36 @@
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(()=> {
   chrome.history.search({
     text: "",
-    maxResults: "10000"
-  }, function(array of HistoryItem items){
-      chrome.storage.sync.set({history: items});
-      var myBias = setMyBias(items);
-      setBias(myBias, items);
+    maxResults: 110
+    }, (items) => {
+      // console.log(items);
       setFreq(items);
+      setBias();
     })
-  }
-);
 
-chrome.tabs.onUpdated.addListener(function(integer tabID, object changeInfo, Tab tab) {
-  chrome.storage.sync.get('site_bias_map', function(bias) {
-    if(bias.contains(changeInfo.url)){
+  chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab) {
       chrome.storage.sync.get('site_freq_map', function(freq){
-        freq[bias] += 1;
-      }
-      alert("Bad Site");
-    }
-  })
-});
+        freq[changeInfo.url] += 1;
+        updateBias();
+      })
+      chrome.storage.sync.get('site_bias_map', function(bias){
+        if(bias[changeInfo.url] > 40 || bias[changeInfo.url] < -40){
+          alert("Bad Site");
+        }
+      })
+  });
 
-function setBias(){
+  function setBias(myBias, items){
+    // console.log(items);
+  }
 
-}
+  function setFreq(items){
+    // console.log(items);
+    var freq;
+    updateBiasScore(freq);
+  }
 
-function setFreq(){
+  function updateBiasScore(freq){
 
-}
+  }
+})
