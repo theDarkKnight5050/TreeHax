e21d963ff854a72af40d680fc7e93ccb7726fafa
Ahmed <aiabdalla5050@gmail.com>
2019-02-17 05:15:36 -0800

    Simple UI

--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,9 @@
+console.log("Reached popup.js");
 let setBias = document.getElementById("bias");
+let suggestions = document.getElementById("suggested")
 
 chrome.storage.sync.get('bias', function(data){
   setBias.innerHTML = data.bias + "";
 });
+
+suggestions.innerHTML = "Siah Yong I think?"
